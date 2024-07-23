fa590f2d432d66b5df68b7634fa01fd9be93e45d
Shivansh270 <gupta.shivansh2709@gmail.com>
2024-07-23 19:38:13 +0530

    backend of the auth app

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,53 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+import bcryptjs from "bcryptjs";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "User does not exist" },
+        { status: 400 }
+      );
+    }
+    console.log("user exists");
+
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json({ error: "Invalid password" }, { status: 400 });
+    }
+
+    console.log(user);
+
+    const tokenData = {
+      id: user._id,
+      email: user.email,
+      password: user.password,
+    };
+
+    const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    //now here we have a variable of type next response, we can directly access cookies
+    const response = NextResponse.json({
+      message: "Logged in success",
+      success: true,
+    });
+
+    response.cookies.set("token", token, { httpOnly: true });
+
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
