c7cc6ade66cdf516622d0a92bee0d26780909a63
Shivansh270 <gupta.shivansh2709@gmail.com>
2024-08-04 23:05:39 +0530

    login and profile page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+"use client";
+import axios from "axios";
+import toast from "react-hot-toast";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
-function page() {
+function Signup() {
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.post("/api/users/login", formData);
+      console.log("Login success", res.data);
+      toast.success("Login success");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log(error);
+      console.log("Login failed");
+    }
+  };
+
+  useEffect(() => {
+    if (
+      formData.email.length > 0 &&
+      formData.password.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [formData]);
+
   return (
-    <div>login page</div>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>{loading ? "Processing" : "Login"}</h1>
+      <hr />
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        id="email"
+        type="text"
+        value={formData.email}
+        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        id="password"
+        type="password"
+        value={formData.password}
+        onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        onClick={onLogin}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        {buttonDisabled ? "No login" : "login"}
+      </button>
+      <Link href="/signup">Visit login page</Link>
+    </div>
+  );
 }
 
-export default page
+export default Signup;
